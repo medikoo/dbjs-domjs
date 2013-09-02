ca9c8682a7ba32a77153806bd7940b61ba392274
Mariusz Nowak <mariusz@medikoo.com>
2013-09-02 16:24:14 +0200

    Wider dbjs types support in input handling

--- input.js
+++ input.js
@@ -1,11 +1,14 @@
 'use strict';
 
+var types = require('es5-ext/object/primitive-set')('namespace', 'prototype',
+	'object', 'relation');
+
 module.exports = function (domjs) {
 	var input = domjs.ns.input;
 
 	domjs.ns.input = function (attrs) {
 		var rel = attrs && attrs.dbjs;
-		if (!rel || (rel._type_ !== 'relation')) {
+		if (!rel || !types[rel._type_]) {
 			return input.apply(this, arguments);
 		}
 		return rel.toDOMInput(domjs.document, attrs).dom;
