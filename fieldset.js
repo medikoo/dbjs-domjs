0c8f7bd42472b3c468d50223f309ae120860ba12
Mariusz Nowak <mariusz@medikoo.com>
2013-09-03 20:56:40 +0200

    Fieldset handler

---
+++ fieldset.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var Fieldset = require('dbjs-dom/input/utils/fieldset')
+
+  , isArray = Array.isArray;
+
+module.exports = function (domjs) {
+	var fieldset = domjs.ns.fieldset;
+
+	domjs.ns.fieldset = function (attrs) {
+		var obj = attrs && attrs.dbjs;
+		if (!obj) return fieldset.apply(this, arguments);
+		if (isArray(obj)) {
+			delete attrs.dbjs;
+			return new Fieldset(domjs.document, obj, attrs);
+		}
+		if (!obj.toDOMFieldset) return fieldset.apply(this, arguments);
+		return obj.toDOMFieldset(domjs.document, attrs).dom;
+	};
+};
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
 
-var field = require('./field')
-  , input = require('./input');
+var field    = require('./field')
+  , fieldset = require('./fieldset')
+  , input    = require('./input');
 
 module.exports = function (domjs) {
 	field(domjs);
+	fieldset(domjs);
 	input(domjs);
 };
