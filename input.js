4abfe52ee561a43f26084a202ba227de3fb14c3f
Mariusz Nowak <mariusz@medikoo.com>
2014-01-18 21:52:51 +0100

    Allow dbjs types as input objects

--- input.js
+++ input.js
@@ -1,14 +1,17 @@
 'use strict';
 
-var validObservableValue = require('observable-value/valid-observable-value');
+var isObservableValue = require('observable-value/is-observable-value')
+  , isDbjsType        = require('dbjs/is-dbjs-type');
 
 module.exports = function (domjs) {
 	var input = domjs.ns.input;
 
 	domjs.ns.input = function (attrs) {
 		if (attrs && (attrs.dbjs != null)) {
-			return validObservableValue(attrs.dbjs)
-				.toDOMInput(domjs.document, attrs).dom;
+			if (!isObservableValue(attrs.dbjs) && !isDbjsType(attrs.dbjs)) {
+				throw new TypeError(attrs.dbjs + " is not dbjs observable or type");
+			}
+			return attrs.dbjs.toDOMInput(domjs.document, attrs).dom;
 		}
 		return input.apply(this, arguments);
 	};
