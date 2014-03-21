499bb0147ef56052cdac3d525933a2e19ee5332c
Mariusz Nowak <mariusz@medikoo.com>
2014-03-21 11:46:05 +0100

    Dedicated handling for input[type=hidden]

--- input.js
+++ input.js
@@ -7,9 +7,22 @@ module.exports = function (domjs) {
 	var input = domjs.ns.input;
 
 	domjs.ns.input = function (attrs) {
+		var isType;
 		if (attrs && (attrs.dbjs != null)) {
-			if (!isObservableValue(attrs.dbjs) && !isDbjsType(attrs.dbjs)) {
-				throw new TypeError(attrs.dbjs + " is not dbjs observable or type");
+			if (!isObservableValue(attrs.dbjs)) {
+				isType = isDbjsType(attrs.dbjs);
+				if (!isType) {
+					throw new TypeError(attrs.dbjs + " is not dbjs observable or type");
+				}
+			}
+			if (attrs.type === 'hidden') {
+				if (!isType) attrs.name = attrs.dbjs.dbId;
+				if (attrs.value != null) {
+					attrs.value = [isType ? attrs.dbjs : attrs.dbjs.descriptor.type]
+						.toInputValue(attrs.value);
+				}
+				delete attrs.dbjs;
+				return input.apply(this, arguments);
 			}
 			return attrs.dbjs.toDOMInput(domjs.document, attrs).dom;
 		}
