d67c8bb9264940604bcb53263203d773574e72b8
Mariusz Nowak <medyk@medikoo.com>
2014-11-24 16:38:28 +0100

    Assure normalization of custom fieldsets

--- fieldset.js
+++ fieldset.js
@@ -5,16 +5,16 @@ var Fieldset = require('dbjs-dom/input/utils/fieldset').Fieldset
   , isArray = Array.isArray;
 
 module.exports = function (domjs) {
-	var fieldset = domjs.ns.fieldset;
+	var fieldset = domjs.ns.fieldset, normalize = domjs.ns.normalize;
 
 	return (domjs.ns.fieldset = function (attrs) {
 		var obj = attrs && attrs.dbjs;
 		if (!obj) return fieldset.apply(this, arguments);
 		if (isArray(obj)) {
 			delete attrs.dbjs;
-			return new Fieldset(domjs.document, obj, attrs);
+			return normalize((new Fieldset(domjs.document, obj, attrs)).dom);
 		}
 		if (!obj.toDOMFieldset) return fieldset.apply(this, arguments);
-		return obj.toDOMFieldset(domjs.document, attrs).dom;
+		return normalize(obj.toDOMFieldset(domjs.document, attrs).dom);
 	});
 };
