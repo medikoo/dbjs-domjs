b3add9873435b119455abb6032a22288ca5b1894
Mariusz Nowak <medyk@medikoo.com>
2015-06-15 12:28:08 +0200

    Support field for dbjs Type input

--- field.js
+++ field.js
@@ -1,14 +1,19 @@
 'use strict';
 
-var validObservableValue = require('observable-value/valid-observable-value');
+var isObservableValue = require('observable-value/is-observable-value')
+  , isDbjsType        = require('dbjs/is-dbjs-type');
 
 module.exports = function (domjs) {
 	var input = domjs.ns.input;
 
 	return (domjs.ns.field = function (attrs) {
+		var isType;
 		if (attrs && (attrs.dbjs != null)) {
-			return validObservableValue(attrs.dbjs)
-				.toDOMInputComponent(domjs.document, attrs).dom;
+			if (!isObservableValue(attrs.dbjs)) {
+				isType = isDbjsType(attrs.dbjs);
+				if (!isType) throw new TypeError(attrs.dbjs + " is not dbjs observable or type");
+			}
+			return attrs.dbjs.toDOMInputComponent(domjs.document, attrs).dom;
 		}
 		return input.apply(this, arguments);
 	});
